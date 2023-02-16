f311e897a3842838a64d2998d1857136ec68a8bc
Jonny Burger <jonathanburger11@gmail.com>
2023-02-16 14:30:50 +0100

    limit muxers

--- build.mjs
+++ build.mjs
@@ -18,8 +18,14 @@ if (fs.existsSync("ffmpeg")) {
   });
 }
 
+execSync("rm -rf remotion", {
+  cwd: "ffmpeg",
+  stdio: "inherit",
+});
+
 execSync("git checkout n5.1.1", {
   cwd: "ffmpeg",
+  stdio: "inherit",
 });
 
 execSync(
@@ -30,8 +36,53 @@ execSync(
     "--disable-static",
     "--enable-shared",
     "--disable-ffplay",
+    "--disable-avdevice",
+    "--disable-filters",
+    "--enable-filter=aformat",
+    "--enable-filter=atrim",
+    "--enable-filter=adelay",
+    "--enable-filter=anullsrc",
+    "--enable-filter=atempo",
+    "--enable-filter=apad",
+    "--enable-filter=amerge",
+    "--enable-filter=concat",
+    "--enable-filter=tinterlace",
+    "--enable-filter=fieldorder",
+    "--enable-filter=pan",
+    "--enable-filter=volume",
+    "--disable-indevs",
+    "--disable-doc",
+    "--enable-nonfree",
+    "--disable-encoders",
+    "--enable-encoder=opus",
+    "--enable-encoder=mp3",
+    "--enable-encoder=aac",
+    "--enable-encoder=pcm_s16le",
+    "--disable-muxers",
+    "--enable-muxer=h264",
+    "--enable-muxer=h265",
+    "--enable-muxer=webm",
+    "--enable-muxer=s16le",
+    "--enable-muxer=opus",
+    "--enable-muxer=mp4",
+    "--enable-muxer=mp3",
+    "--enable-muxer=mov",
+    "--enable-muxer=mkvtimestamp_v2",
+    "--enable-muxer=matroska",
+    "--enable-muxer=hevc",
+    "--enable-muxer=h264",
+    "--enable-muxer=gif",
   ].join(" "),
   {
     cwd: "ffmpeg",
+    stdio: "inherit",
   }
 );
+execSync("make", {
+  cwd: "ffmpeg",
+  stdio: "inherit",
+});
+execSync("make install", {
+  cwd: "ffmpeg",
+  stdio: "inherit",
+});
