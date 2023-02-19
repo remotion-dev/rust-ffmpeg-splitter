8a9810f2f8dac1b2590cfd88d43e09b7c50a165f
Jonny Burger <jonathanburger11@gmail.com>
2023-02-19 10:27:17 +0100

    fix windows build?

--- generate-bindings.mjs
+++ generate-bindings.mjs
@@ -2,18 +2,23 @@ import { execSync } from "child_process";
 import path from "path";
 import fs from "fs";
 
-execSync("cargo build --release", {
-  env: {
-    ...process.env,
-    FFMPEG_DIR: path.join(process.cwd(), "ffmpeg", "remotion"),
+const isWindows = process.argv[2] === "windows";
 
-    CPATH:
-      process.platform === "darwin"
-        ? "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include"
-        : undefined,
-  },
-  stdio: "inherit",
-});
+execSync(
+  `cargo build --release ${isWindows ? "--target=x86_64-pc-windows-gnu" : ""}`,
+  {
+    env: {
+      ...process.env,
+      FFMPEG_DIR: path.join(process.cwd(), "ffmpeg", "remotion"),
+
+      CPATH:
+        process.platform === "darwin"
+          ? "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include"
+          : undefined,
+    },
+    stdio: "inherit",
+  }
+);
 
 const bindings = path.join(process.cwd(), "target", "release", "build");
 const folders = fs.readdirSync(bindings);
