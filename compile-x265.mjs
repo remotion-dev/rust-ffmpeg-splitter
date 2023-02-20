a4ed4fa8c05c1b5e35b2d32623b480709e1ed62e
Jonny Burger <jonathanburger11@gmail.com>
2023-02-20 19:44:00 +0100

    Update compile-x265.mjs

--- compile-x265.mjs
+++ compile-x265.mjs
@@ -1,7 +1,8 @@
-import fs, { existsSync, unlinkSync } from "fs";
+import fs, { existsSync, readFileSync, unlinkSync } from "fs";
 import path from "path";
 import { execSync } from "child_process";
 import { PREFIX } from "./const.mjs";
+import { compileFunction } from "vm";
 
 export const enableX265 = (isMusl, isWindows) => {
   const extraCFlags = [
@@ -29,8 +30,9 @@ export const enableX265 = (isMusl, isWindows) => {
       "cmake",
       '-DCMAKE_INSTALL_PREFIX="remotion"',
       "-DENABLE_SHARED:BOOL=OFF",
-      "-DSTATIC_LINK_CRT:BOOL=ON",
-      "-DENABLE_CLI:BOOL=OFF",
+      "-DCMAKE_BUILD_TYPE=Release",
+      "-DSTATIC_LINK_CRT:BOOL=OFF",
+      "-DENABLE_CLI:BOOL=ON",
       "source",
     ]
       .filter(Boolean)
@@ -38,43 +40,58 @@ export const enableX265 = (isMusl, isWindows) => {
     {
       cwd: "x265",
       stdio: "inherit",
-      env: {
-        ...process.env,
-        CMAKE_CROSSCOMPILING: isWindows ? "ON" : undefined,
-        CMAKE_C_COMPILER: isWindows ? "x86_64-w64-mingw32-gcc" : undefined,
-        CMAKE_CXX_COMPILER: isWindows ? "x86_64-w64-mingw32-g++" : undefined,
-        CFLAGS: extraCFlags.join(" "),
-        CXXFLAGS: isMusl ? "-static-libgcc -static-libstdc++" : undefined,
-      },
     }
   );
 
   execSync("make", {
     cwd: "x265",
     stdio: "inherit",
+    env: {
+      CMAKE_CROSSCOMPILING: isWindows ? "ON" : undefined,
+      CMAKE_C_COMPILER: isWindows ? "x86_64-w64-mingw32-gcc" : undefined,
+      CMAKE_CXX_COMPILER: isWindows ? "x86_64-w64-mingw32-g++" : undefined,
+      CMAKE_RC_COMPILER: isWindows ? "x86_64-w64-mingw32-windres" : undefined,
+      CMAKE_RANLIB: isWindows ? "x86_64-w64-mingw32-ranlib" : undefined,
+      CMAKE_SYSTEM_NAME: isWindows ? "Windows" : undefined,
+      CMAKE_ASM_YASM_COMPILER: isWindows ? "yasm" : undefined,
+      CFLAGS: extraCFlags.join(" "),
+    },
   });
   execSync("make install", {
     cwd: "x265",
     stdio: "inherit",
   });
 
-  execSync("sed -e 's/prefix=\\/.*/prefix=remotion/g' x265.pc > xx.pc", {
-    cwd: "x265/remotion/lib/pkgconfig",
-    stdio: "inherit",
-  });
+  const x265 = readFileSync("x265/remotion/lib/pkgconfig/x265.pc", "utf8");
+  console.log("pkgconfig/x265.pc is:", x265);
+  const lines = x265.split("\n");
+  const linesPkg = lines
+    .map((line) => {
+      if (line.startsWith("prefix=")) {
+        return "prefix=remotion";
+      }
+      if (line.startsWith("exec_prefix=")) {
+        return "exec_prefix=remotion";
+      }
+      if (line.startsWith("Libs:")) {
+        return line + " -lc++ -ldl";
+      }
+      if (line.startsWith("Libs.private")) {
+        return null;
+      }
 
-  execSync("rm x265.pc", {
-    cwd: "x265/remotion/lib/pkgconfig",
-    stdio: "inherit",
-  });
+      return line;
+    })
+    .filter((l) => l !== null)
+    .join("\n");
 
-  execSync("mv xx.pc x265.pc", {
-    cwd: "x265/remotion/lib/pkgconfig",
-    stdio: "inherit",
-  });
+  console.log("Replacing it with:", linesPkg);
+  fs.writeFileSync("x265/remotion/lib/pkgconfig/x265.pc", linesPkg);
 
   execSync("cp -r " + PREFIX + " ../", {
     cwd: "x265",
     stdio: "inherit",
   });
 };
+
+enableX265(false, false);
