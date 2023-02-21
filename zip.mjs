d2074ce0d8c11c6a24eacccb14a22774fae0c7c9
Jonny Burger <jonathanburger11@gmail.com>
2023-02-21 19:52:57 +0100

    delete .lib files we don't need them, also delete .def files

--- zip.mjs
+++ zip.mjs
@@ -1,5 +1,5 @@
 import { execSync } from "child_process";
-import { readdirSync, renameSync } from "fs";
+import { readdirSync, renameSync, unlinkSync } from "fs";
 import path from "path";
 import { PREFIX } from "./const.mjs";
 
@@ -7,14 +7,25 @@ const isWindows = process.argv[2] === "windows";
 
 if (isWindows) {
   const remotionBinDir = path.join(process.cwd(), "remotion", "bin");
-  const files = readdirSync(remotionBinDir);
-  for (const file of files) {
-    if (file.endsWith(".dll") || file.endsWith(".lib")) {
+  const remotionLibDir = path.join(process.cwd(), "remotion", "lib");
+  const binFiles = readdirSync(remotionBinDir);
+  for (const file of binFiles) {
+    if (file.endsWith(".lib")) {
+      const oldPath = path.join(remotionBinDir, file);
+      unlinkSync(oldPath);
+    } else if (file.endsWith(".dll")) {
       const oldPath = path.join(remotionBinDir, file);
       const newPath = path.join(remotionBinDir, "..", "lib", file);
 
       console.log(oldPath, newPath);
       renameSync(oldPath, newPath);
+    }
+  }
+
+  const libFiles = readdirSync(remotionLibDir);
+  for (const file of libFiles) {
+    if (file.endsWith(".def")) {
+      unlinkSync(path.join(remotionLibDir, file));
     }
   }
 }
