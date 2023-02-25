57e445586a7c54a47057dc30419672fdb597f309
Jonny Burger <jonathanburger11@gmail.com>
2023-02-25 17:45:15 +0100

    Update test-ffmpeg.mjs

--- test-ffmpeg.mjs
+++ test-ffmpeg.mjs
@@ -1,6 +1,5 @@
 import { execSync, spawnSync } from "child_process";
 import path from "path";
-import { test } from "node:test";
 import assert from "assert";
 
 const lib = path.join(process.cwd(), "remotion", "lib");
@@ -23,87 +22,77 @@ const env =
 
 const ffmpegBinary = path.join(process.cwd(), "remotion", "bin", "ffmpeg");
 
-test("Should be able to run FFmpeg", () => {
-  const exit = spawnSync(ffmpegBinary, ["-buildconf"], {
+const exit = spawnSync(ffmpegBinary, ["-buildconf"], {
+  env,
+});
+assert(exit.status === 0);
+
+const exit = spawnSync(
+  ffmpegBinary,
+  ["-i", "sample-5s.webm", "-t", "1", "out-test.mp4", "-y"],
+  {
     env,
-  });
-  assert(exit.status === 0);
-});
+  }
+);
+assert(exit.status === 0);
 
-test("Should be able to convert webm to mp4", () => {
-  const exit = spawnSync(
-    ffmpegBinary,
-    ["-i", "sample-5s.webm", "-t", "1", "out-test.mp4", "-y"],
-    {
-      env,
-    }
-  );
-  assert(exit.status === 0);
-});
+const exit = spawnSync(
+  ffmpegBinary,
+  [
+    "-i",
+    "sample.mp4",
+    "-t",
+    "1",
+    "-c:v",
+    "libvpx",
+    "-c:a",
+    "libopus",
+    "out-test.webm",
+    "-y",
+  ],
+  {
+    env,
+    stdio: "inherit",
+  }
+);
+assert(exit.status === 0);
 
-test("Should be able to convert mp4 to webm", () => {
-  const exit = spawnSync(
-    ffmpegBinary,
-    [
-      "-i",
-      "sample.mp4",
-      "-t",
-      "1",
-      "-c:v",
-      "libvpx",
-      "-c:a",
-      "libopus",
-      "out-test.webm",
-      "-y",
-    ],
-    {
-      env,
-      stdio: "inherit",
-    }
-  );
-  assert(exit.status === 0);
-});
+const exit = spawnSync(
+  ffmpegBinary,
+  [
+    "-i",
+    "sample.mp4",
+    "-f",
+    "image2",
+    "-frames:v",
+    "1",
+    "-vcodec",
+    "png",
+    "out-test.png",
+    "-y",
+  ],
+  {
+    env,
+  }
+);
+assert(exit.status === 0);
 
-test("Should be able to extract PNG from video", () => {
-  const exit = spawnSync(
-    ffmpegBinary,
-    [
-      "-i",
-      "sample.mp4",
-      "-f",
-      "image2",
-      "-frames:v",
-      "1",
-      "-vcodec",
-      "png",
-      "out-test.png",
-      "-y",
-    ],
-    {
-      env,
-    }
-  );
-  assert(exit.status === 0);
-});
-
-test("Should be able to extract JPEG from video", () => {
-  const exit = spawnSync(
-    ffmpegBinary,
-    [
-      "-i",
-      "sample.mp4",
-      "-f",
-      "image2",
-      "-frames:v",
-      "1",
-      "-vcodec",
-      "mjpeg",
-      "out-test.png",
-      "-y",
-    ],
-    {
-      env,
-    }
-  );
-  assert(exit.status === 0);
-});
+const exit = spawnSync(
+  ffmpegBinary,
+  [
+    "-i",
+    "sample.mp4",
+    "-f",
+    "image2",
+    "-frames:v",
+    "1",
+    "-vcodec",
+    "mjpeg",
+    "out-test.png",
+    "-y",
+  ],
+  {
+    env,
+  }
+);
+assert(exit.status === 0);
