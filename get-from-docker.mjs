a2e474e131ddf8983a2c97bdc5c2cf551c832fdf
Jonny Burger <jonathanburger11@gmail.com>
2023-04-26 15:39:13 +0200

    fix

--- get-from-docker.mjs
+++ get-from-docker.mjs
@@ -1,10 +1,12 @@
 import { execSync } from "child_process";
 
 const containerName = process.argv[2];
+const path = process.argv[3];
+
 const result = execSync(`docker run -d ${containerName}`);
 
-const id = result.toString();
+const id = result.toString().trim();
 
-execSync("docker cp " + id.trim() + ":/app/ffmpeg.tar.gz ffmpeg.tar.gz");
+execSync(`docker cp ${id}:${path} ffmpeg.tar.gz`);
 
 execSync("docker stop " + result);
