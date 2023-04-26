9057c205ae571a5a40f8535b4ebff3b7ee6326c9
Jonny Burger <jonathanburger11@gmail.com>
2023-04-26 14:59:56 +0200

    fix there

--- get-from-docker.mjs
+++ get-from-docker.mjs
@@ -1,6 +1,7 @@
 import { execSync } from "child_process";
 
-const result = execSync("docker run -d aarch-musl");
+const containerName = process.argv[2];
+const result = execSync(`docker run -d ${containerName}`);
 
 const id = result.toString();
 
