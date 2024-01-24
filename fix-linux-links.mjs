2d1f15176d3935aaac7c7f07b842da0d55bfb0db
Jonny Burger <jonathanburger11@gmail.com>
2024-01-24 17:11:53 +0100

    Update fix-linux-links.mjs

--- fix-linux-links.mjs
+++ fix-linux-links.mjs
@@ -1,12 +1,13 @@
 import { execSync } from "child_process";
+import { PREFIX } from "./const.mjs";
 
 export const fixLinuxLinks = () => {
-  execSync('patchelf --force-rpath --set-rpath "$ORIGIN" ffmpeg', {
-    cwd: "remotion/bin",
+  execSync('patchelf --force-rpath --set-rpath "$ORIGIN" ./ffmpeg', {
+    cwd: "ffmpeg/" + PREFIX + "/bin",
     stdio: "inherit",
   });
-  execSync('patchelf --force-rpath --set-rpath "$ORIGIN" ffprobe', {
-    cwd: "remotion/bin",
+  execSync('patchelf --force-rpath --set-rpath "$ORIGIN" ./ffprobe', {
+    cwd: "ffmpeg/" + PREFIX + "/bin",
     stdio: "inherit",
   });
 };
