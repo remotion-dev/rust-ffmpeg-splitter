3d1e8204da97599cfe1a82e0e1645747f32cd3a6
Jonny Burger <jonathanburger11@gmail.com>
2024-02-06 11:37:07 +0100

    Update compile-ffmpeg.mjs

--- compile-ffmpeg.mjs
+++ compile-ffmpeg.mjs
@@ -127,7 +127,7 @@ enableX265(isMusl, isWindows);
 enableLibMp3Lame(isWindows);
 enableOpus(isWindows);
 
-const TAG = "f30b620e98ca35eb669fae4fbe4911b779c8413d";
+const TAG = "0b352e350e773673f11ea380f3507923c70e1175";
 
 if (fs.existsSync("ffmpeg")) {
   execSync("git stash", {
