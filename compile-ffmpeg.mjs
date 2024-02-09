fc8efe13cb99462548aaba291d2a797e24f7dc10
Jonny Burger <jonathanburger11@gmail.com>
2024-02-09 11:04:04 +0100

    new repro

--- compile-ffmpeg.mjs
+++ compile-ffmpeg.mjs
@@ -146,9 +146,6 @@ if (fs.existsSync("ffmpeg")) {
     cwd: "ffmpeg",
     stdio: "inherit",
   });
-  execSync("git apply prores.patch --directory ffmpeg", {
-    stdio: "inherit",
-  });
   execSync("git apply aac.patch --directory ffmpeg", {
     stdio: "inherit",
   });
@@ -158,9 +155,6 @@ if (fs.existsSync("ffmpeg")) {
   });
   execSync(`git checkout ${TAG}`, {
     cwd: "ffmpeg",
-    stdio: "inherit",
-  });
-  execSync("git apply prores.patch --directory ffmpeg", {
     stdio: "inherit",
   });
   execSync("git apply aac.patch --directory ffmpeg", {
