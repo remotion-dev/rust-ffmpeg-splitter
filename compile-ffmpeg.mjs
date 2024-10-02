dab36324dd9f0fd16695186ad7af12ff2ef9dc11
Jonny Burger <jonathanburger11@gmail.com>
2024-10-02 10:23:42 +0200

    Update compile-ffmpeg.mjs

--- compile-ffmpeg.mjs
+++ compile-ffmpeg.mjs
@@ -158,9 +158,6 @@ if (fs.existsSync("ffmpeg")) {
     cwd: "ffmpeg",
     stdio: "inherit",
   });
-  execSync("git apply prores.patch --directory ffmpeg", {
-    stdio: "inherit",
-  });
   execSync("git apply aac.patch --directory ffmpeg", {
     stdio: "inherit",
   });
